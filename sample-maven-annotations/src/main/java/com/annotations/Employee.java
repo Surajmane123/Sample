6fa3b418b5b46f1a45d8cf94ed8ee80c28272a5e
Suraj <Suraj@Suraj>
2022-11-25 17:22:15 +0530

    commit

---
+++ sample-maven-annotations/src/main/java/com/annotations/Employee.java
@@ -0,0 +1,47 @@
+package com.annotations;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table()
+public class Employee {
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	private int empno;
+	@Column
+	private String name;
+	@Column
+	private String address;
+	
+	
+	public Employee(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	
+}
